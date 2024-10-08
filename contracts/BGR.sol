0c8723aaf0e1d3af32cd7be4bfc6f2ed2f6c3b80
kaz <87490841+0xkaz@users.noreply.github.com>
2024-10-08 14:56:58 +0800

    =0.8.27

--- contracts/BGR.sol
+++ contracts/BGR.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.27;
+pragma solidity =0.8.27;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
